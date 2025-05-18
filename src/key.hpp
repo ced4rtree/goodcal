0a06173c5609e63e1d96d06bb980aedf5a1cd1ce
ced4rtree <usernamesarenoteasy@proton.me>
2025-05-18 12:23:35 -0600

    implemented calendar navigation

---
+++ src/key.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <ncurses.h>
+
+constexpr const char WASD_UP    = 'w';
+constexpr const char WASD_LEFT  = 'a';
+constexpr const char WASD_DOWN  = 's';
+constexpr const char WASD_RIGHT = 'd';
+
+constexpr const char VIM_LEFT  = 'h';
+constexpr const char VIM_DOWN  = 'j';
+constexpr const char VIM_UP    = 'k';
+constexpr const char VIM_RIGHT = 'l';
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "date.hpp"
 #include "formatting.hpp"
+#include "key.hpp"
 #include <chrono>
 #include <cmath>
 #include <curses.h>
@@ -44,6 +45,9 @@ int main(void) {
     std::string date_header = std::format("{}", now.month()) + " " +
                               std::to_string(static_cast<int>(now.year()));
 
+    // stuff for interactive date selection
+    unsigned int selected_day = static_cast<unsigned>(now.day());
+
     // event loop
     while (true) {
         // print the calendar
@@ -56,6 +60,10 @@ int main(void) {
             // offset to account for the month not starting on Sunday
             int day_offset = day + first_day_of_month(now).c_encoding();
             double weekdays_more = DAYS_IN_WEEK + 0.00001;
+            if (day == selected_day) {
+                wattron(window, A_REVERSE);
+                // attron(A_BLINK);
+            }
             mvwprintw(
                 window,
                 std::floor(day_offset / weekdays_more)
@@ -66,17 +74,43 @@ int main(void) {
                              : day_offset % (int)DAYS_IN_WEEK),
                 "%d", day
             );
+            wattroff(window, A_REVERSE);
+            // attroff(A_BLINK);
         }
 
         // Handle key processing
         int key = wgetch(window);
-
         switch (key) {
             case 'q':
                 // close window and quit the program
                 refresh();
                 wrefresh(window);
                 return endwin() && delwin(window);
+            // interactively navigate the calendar
+            case WASD_UP:
+            case VIM_UP:
+                if (selected_day > DAYS_IN_WEEK) {
+                    selected_day -= DAYS_IN_WEEK;
+                }
+                break;
+            case WASD_DOWN:
+            case VIM_DOWN:
+                if (selected_day + DAYS_IN_WEEK <= days_in_month(now)) {
+                    selected_day += DAYS_IN_WEEK;
+                }
+                break;
+            case WASD_LEFT:
+            case VIM_LEFT:
+                if (selected_day > 1) {
+                    selected_day--;
+                }
+                break;
+            case WASD_RIGHT:
+            case VIM_RIGHT:
+                if (selected_day < days_in_month(now)) {
+                    selected_day++;
+                }
+                break;
         }
 
         refresh();
